501954988f18351e47118edf11c403523c77f167
George Coney <gconey@cybertron.co.uk>
2018-11-06 10:07:47 +0000

    Add files via upload

---
+++ max-value-validator.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input, forwardRef, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
+
+@Directive({
+    selector: '[maxvalue]',
+    providers: [
+        { provide: NG_VALIDATORS, useExisting: forwardRef(() => MaxValueValidator), multi: true }
+    ],
+    host: { '[attr.max]': 'maxValue ? maxValue : 0' }
+})
+export class MaxValueValidator implements Validator, OnChanges {
+
+    @Input('maxvalue') maxValue: number;
+
+    registerOnValidatorChange(fn: () => void): void { this._onChange = fn; }
+    private _onChange: () => void;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ('maxValue' in changes) {
+            if (this._onChange) this._onChange();
+        }
+    }
+
+    validate(control: AbstractControl): { [key: string]: any } {
+        const givenvalue = control.value;
+        let validationResult = null;
+
+        const maxValue = this.maxValue;
+        if (maxValue && givenvalue > maxValue) {
+            validationResult = validationResult || {};
+            validationResult = { maxvalue: { requiredValue: this.maxValue }};
+        }
+
+        return validationResult;
+    }
+}
---
+++ min-value-validator.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input, forwardRef, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
+
+@Directive({
+    selector: '[minvalue]',
+    providers: [
+        { provide: NG_VALIDATORS, useExisting: forwardRef(() => MinValueValidator), multi: true }
+    ],
+    host: { '[attr.min]': 'minValue ? minValue : 0' }
+})
+export class MinValueValidator implements Validator, OnChanges {
+
+    @Input('minvalue') minValue: number;
+
+    registerOnValidatorChange(fn: () => void): void { this._onChange = fn; }
+    private _onChange: () => void;
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ('minValue' in changes) {
+            if (this._onChange) this._onChange();
+        }
+    }
+
+    validate(control: AbstractControl): { [key: string]: any } {
+        const givenvalue = control.value;
+        let validationResult = null;
+
+        const minValue = this.minValue;
+        if (minValue && givenvalue < minValue) {
+            validationResult = validationResult || {};
+            validationResult = { minvalue: { requiredValue: this.minValue } };
+        }
+
+        return validationResult;
+    }
+}
